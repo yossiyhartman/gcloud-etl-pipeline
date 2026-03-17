789a960ce4177250987f3bdfa7e79916f3282f30
Yossi Hartman <yossiyhartman@gmail.com>
2026-03-17 09:23:18 +0100

    fix(ci): add iam block

--- infra/pipeline/main.tf
+++ infra/pipeline/main.tf
@@ -18,34 +18,34 @@ module "project" {
   project_name = var.project_name
 }
 
-# module "iam" {
-#   source                = "./modules/iam"
-#   project_name          = var.project_name
-#   service_account_email = var.service_acount_email
-#   db_name               = var.db_name
-#   db_user               = var.db_user
-#   db_password           = var.db_password
-#   depends_on            = [module.project]
+module "iam" {
+  source                = "./modules/iam"
+  project_name          = var.project_name
+  service_account_email = var.service_acount_email
+  db_name               = var.db_name
+  db_user               = var.db_user
+  db_password           = var.db_password
+  depends_on            = [module.project]
+}
+
+# module "artifact" {
+#   source           = "./modules/artifact"
+#   project_name     = var.project_name
+#   artifact_repo_id = var.artifact_repo_id
+#   depends_on       = [module.project, module.iam]
 # }
 
-# # module "artifact" {
-# #   source           = "./modules/artifact"
-# #   project_name     = var.project_name
-# #   artifact_repo_id = var.artifact_repo_id
-# #   depends_on       = [module.project]
-# # }
-
-# # module "storage" {
-# #   source       = "./modules/storage"
-# #   project_name = var.project_name
-# #   bucket_name  = "yossi-data-bucket"
-# #   depends_on   = [module.project]
-# # }
+# module "storage" {
+#   source       = "./modules/storage"
+#   project_name = var.project_name
+#   bucket_name  = "yossi-data-bucket"
+#   depends_on   = [module.project, module.iam]
+# }
 
 # module "database" {
 #   source                = "./modules/database"
 #   project_name          = var.project_name
 #   service_account_email = var.service_acount_email
 #   instance_name         = var.instance_name
-#   depends_on            = [module.project]
+#   depends_on            = [module.project, module.iam]
 # }
--- infra/pipeline/variables.tf
+++ infra/pipeline/variables.tf
@@ -6,11 +6,11 @@ variable "project_name" {
   default     = "yossi-infra-ci-project"
 }
 
-# variable "service_acount_email" {
-#   description = "Name of the serviea account"
-#   type        = string
-#   default     = "terraform@yossi-infra-ci-project.iam.gserviceaccount.com"
-# }
+variable "service_acount_email" {
+  description = "Name of the serviea account"
+  type        = string
+  default     = "terraform@yossi-infra-ci-project.iam.gserviceaccount.com"
+}
 
 # variable "artifact_repo_id" {
 #   description = "the name of the repo in the Artifact Repository"
@@ -23,20 +23,20 @@ variable "project_name" {
 #   }
 # }
 
-# variable "db_name" {
-#   description = "Name of the database"
-#   type        = string
-#   sensitive   = false
-# }
+variable "db_name" {
+  description = "Name of the database"
+  type        = string
+  sensitive   = false
+}
 
-# variable "db_user" {
-#   description = "Name of the database user"
-#   type        = string
-#   sensitive   = true
-# }
+variable "db_user" {
+  description = "Name of the database user"
+  type        = string
+  sensitive   = true
+}
 
-# variable "db_password" {
-#   description = "Name of the database user"
-#   type        = string
-#   sensitive   = true
-# }
+variable "db_password" {
+  description = "Name of the database user"
+  type        = string
+  sensitive   = true
+}
