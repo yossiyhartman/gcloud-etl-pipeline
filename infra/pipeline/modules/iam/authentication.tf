220756e4e9d9413dfb133421af9841512fc0efda
Yossi Hartman <yossiyhartman@gmail.com>
2026-03-18 15:59:29 +0100

    fix(ci): order dependencies

--- infra/pipeline/modules/iam/authentication.tf
+++ infra/pipeline/modules/iam/authentication.tf
@@ -11,6 +11,7 @@ resource "google_secret_manager_secret" "db-name-secret-name" {
 resource "google_secret_manager_secret_version" "db-name-secret-value" {
   secret      = google_secret_manager_secret.db-name-secret-name.secret_id
   secret_data = var.db_name
+  depends_on  = [google_secret_manager_secret.db-name-secret-name]
 }
 
 
@@ -27,6 +28,7 @@ resource "google_secret_manager_secret" "db-user-secret-name" {
 resource "google_secret_manager_secret_version" "db-user-secret-value" {
   secret      = google_secret_manager_secret.db-user-secret-name.secret_id
   secret_data = var.db_user
+  depends_on  = [google_secret_manager_secret.db-user-secret-name]
 }
 
 # PASSWORD
@@ -42,4 +44,5 @@ resource "google_secret_manager_secret" "db-password-secret-name" {
 resource "google_secret_manager_secret_version" "db-password-secret-value" {
   secret      = google_secret_manager_secret.db-password-secret-name.secret_id
   secret_data = var.db_password
+  depends_on  = [google_secret_manager_secret.db-password-secret-name]
 }
