d74a9c19cf140aeafe33d5418d5df5406e3e1039
Yossi Hartman <yossiyhartman@gmail.com>
2026-03-18 13:11:23 +0100

    feat(containerization): add docker setup

--- src/gcp_pipeline/ingest/upload_to_bucket.py
+++ src/gcp_pipeline/ingest/upload_to_bucket.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 import pyarrow as pa
@@ -7,15 +8,21 @@ from gcp_pipeline.generate.data_generator import DataGenerator
 
 BUCKET_PATH = os.getenv("BUCKET_PATH")
 
+logger = logging.getLogger()
+
 
 def main():
 
     # Generate some dummy data
+    logger.info("generating data...")
     data = DataGenerator().generate(n_records=1)
+
+    logger.info(data)
 
     # Create a pyarrow table
     table = pa.table(data)
 
+    logger.info("Writing to bucket...")
     # Upload the data to gcp, and partition on the event-time
     ds.write_dataset(
         table,
@@ -24,7 +31,9 @@ def main():
         partitioning=["event_time"],
         existing_data_behavior="overwrite_or_ignore",
     )
+    logger.info("Success!")
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=0)
     main()
