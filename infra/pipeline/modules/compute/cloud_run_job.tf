134fb947053efa00dd37e039f213e054bb32ae14
Yossi Hartman <yossiyhartman@gmail.com>
2026-03-19 11:37:32 +0100

    feat(cloud-run): create a new job fro sql upload

--- infra/pipeline/modules/compute/cloud_run_job.tf
+++ infra/pipeline/modules/compute/cloud_run_job.tf
@@ -1,6 +1,19 @@
 resource "google_cloud_run_v2_job" "upload_to_bucket" {
   project             = var.project_name
-  name                = "gcp-upload"
+  name                = "gcp-upload-to-bucket"
+  location            = "europe-west4"
+  deletion_protection = false
+
+  template {
+    template {
+      service_account = var.service_acount_email
+    }
+  }
+}
+
+resource "google_cloud_run_v2_job" "upload_to_bucket" {
+  project             = var.project_name
+  name                = "gcp-upload-to-sql"
   location            = "europe-west4"
   deletion_protection = false
 
