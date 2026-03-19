a1962bc5dfa070a141876ff439f84d92f7ffed9b
Yossi Hartman <yossiyhartman@gmail.com>
2026-03-19 13:36:34 +0100

    fix(syntax): duplicate resource name

--- infra/pipeline/modules/compute/cloud_run_job.tf
+++ infra/pipeline/modules/compute/cloud_run_job.tf
@@ -23,7 +23,7 @@ resource "google_cloud_run_v2_job" "upload_to_bucket" {
   }
 }
 
-resource "google_cloud_run_v2_job" "upload_to_bucket" {
+resource "google_cloud_run_v2_job" "upload_to_sql" {
   project             = var.project_name
   name                = "gcp-upload-to-sql"
   location            = "europe-west4"
