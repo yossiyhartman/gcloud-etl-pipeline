32b555fe4b8c4c24d6416bc189c7daa686f168b7
Yossi Hartman <yossiyhartman@gmail.com>
2026-03-20 10:50:23 +0100

    refactor(cleanup): cleaning the project structure

--- infra/pipeline/modules/iam/authentication.tf
+++ infra/pipeline/modules/iam/authentication.tf
@@ -9,48 +9,48 @@ terraform {
   }
 }
 
-# NAME
+# # NAME
 
-resource "google_secret_manager_secret" "db_name_secret_name" {
-  secret_id = "db-name"
+# resource "google_secret_manager_secret" "db_name_secret_name" {
+#   secret_id = "db-name"
 
-  replication {
-    auto {}
-  }
-}
+#   replication {
+#     auto {}
+#   }
+# }
 
-resource "google_secret_manager_secret_version" "db_name_secret_value" {
-  secret      = google_secret_manager_secret.db_name_secret_name.id
-  secret_data = var.db_name
-}
+# resource "google_secret_manager_secret_version" "db_name_secret_value" {
+#   secret      = google_secret_manager_secret.db_name_secret_name.id
+#   secret_data = var.db_name
+# }
 
 
-# USER
+# # USER
 
-resource "google_secret_manager_secret" "db_user_secret_name" {
-  secret_id = "db-user"
+# resource "google_secret_manager_secret" "db_user_secret_name" {
+#   secret_id = "db-user"
 
-  replication {
-    auto {}
-  }
-}
+#   replication {
+#     auto {}
+#   }
+# }
 
-resource "google_secret_manager_secret_version" "db_user_secret_value" {
-  secret      = google_secret_manager_secret.db_user_secret_name.id
-  secret_data = var.db_user
-}
+# resource "google_secret_manager_secret_version" "db_user_secret_value" {
+#   secret      = google_secret_manager_secret.db_user_secret_name.id
+#   secret_data = var.db_user
+# }
 
-# PASSWORD
+# # PASSWORD
 
-resource "google_secret_manager_secret" "db_password_secret_name" {
-  secret_id = "db-password"
+# resource "google_secret_manager_secret" "db_password_secret_name" {
+#   secret_id = "db-password"
 
-  replication {
-    auto {}
-  }
-}
+#   replication {
+#     auto {}
+#   }
+# }
 
-resource "google_secret_manager_secret_version" "db_password_secret_value" {
-  secret      = google_secret_manager_secret.db_password_secret_name.id
-  secret_data = var.db_password
-}
+# resource "google_secret_manager_secret_version" "db_password_secret_value" {
+#   secret      = google_secret_manager_secret.db_password_secret_name.id
+#   secret_data = var.db_password
+# }
--- infra/pipeline/modules/iam/variables.tf
+++ infra/pipeline/modules/iam/variables.tf
@@ -1,17 +1,17 @@
-variable "db_name" {
-  description = "Name of the database"
-  type        = string
-  sensitive   = false
-}
+# variable "db_name" {
+#   description = "Name of the database"
+#   type        = string
+#   sensitive   = false
+# }
 
-variable "db_user" {
-  description = "Name of the database user"
-  type        = string
-  sensitive   = true
-}
+# variable "db_user" {
+#   description = "Name of the database user"
+#   type        = string
+#   sensitive   = true
+# }
 
-variable "db_password" {
-  description = "Name of the database password"
-  type        = string
-  sensitive   = true
-}
+# variable "db_password" {
+#   description = "Name of the database password"
+#   type        = string
+#   sensitive   = true
+# }
