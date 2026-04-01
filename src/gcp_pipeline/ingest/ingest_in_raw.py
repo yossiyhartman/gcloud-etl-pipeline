94a81569ffbe455b0ac21ae7e968db67f055d64f
Yossi Hartman <yossiyhartman@gmail.com>
2026-04-01 09:57:28 +0200

    feat(ingesting): rewrite generator class + ingestion layer
    Generator class is refactored. Ingestion layer now outputs ndjson data and upload this to gcp into landing

---
+++ src/gcp_pipeline/ingest/ingest_in_raw.py
@@ -0,0 +1,12 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def ingest_in_raw():
+    """TODO: rewrite this function"""
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    ingest_in_raw()
